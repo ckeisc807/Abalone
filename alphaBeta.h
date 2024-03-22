6904857a02f3817cc714ff5f3ce8cae186bc404e
ckeisc807 <s10855168@gmail.com>
2024-03-22 12:00:59 +0800

    debug evalutation not found

--- alphaBeta.h
+++ alphaBeta.h
@@ -19,7 +19,7 @@ using std::function;
 const int inf=1e9;
 const move empty_move;
 
-pair<EvaluationType, move> AlphaBeta(Abalone &abalone, Evaluation evaluate, int depth = 0, int depth_limit = 3, EvaluationType alpha = -inf, EvaluationType beta = inf) {
+pair<EvaluationType, move> AlphaBeta(Abalone &abalone, Evaluation &evaluate, int depth = 0, int depth_limit = 3, EvaluationType alpha = -inf, EvaluationType beta = inf) {
 	int current_player = abalone.current_player, opponent_player = (current_player == 1) ? 2 : 1;
 	if (abalone.winner || depth == depth_limit) return {evaluate.evaluate(abalone), empty_move};
 	float best_score = -inf, score;
@@ -32,7 +32,6 @@ pair<EvaluationType, move> AlphaBeta(Abalone &abalone, Evaluation evaluate, int 
 		if (score >= beta) return {beta, empty_move};
 		if (score > alpha) alpha = score, best_move = mv;
 	}
-	assert( best_move!=empty_move);
 	return {alpha, best_move};
 }
 
